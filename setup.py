6494050df0006d379a5422bda17dfbf52a11c6b3
mcarter <cartermichael@gmail.com>
2010-07-19 14:17:29 -0700

    Start using version control. rtjp_eventlet 0.3.2

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import os, sys
+
+
+setup(
+    name='rtjp_eventlet',
+    version='0.3.2',
+    author='Michael Carter',
+    author_email='CarterMichael@gmail.com',
+    url='',
+    license='MIT License',
+    description='',
+    long_description='',
+    packages= find_packages(),
+    zip_safe = False,
+    install_requires = [ ],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],        
+)
+
